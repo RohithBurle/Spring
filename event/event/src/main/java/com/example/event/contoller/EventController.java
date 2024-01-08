4c8d76c042b0ba9b5ba98fcaa404a9eda7e050bb
ROHITH <rohithburle16@gmail.com>
2024-01-08 16:42:35 +0530

    Some modifications

--- event/event/src/main/java/com/example/event/contoller/EventController.java
+++ event/event/src/main/java/com/example/event/contoller/EventController.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,11 +13,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.event.model.EventModel;
 import com.example.event.service.EventService;
 
-@Controller
+@RestController
 @RequestMapping("/api")
 public class EventController {
 
